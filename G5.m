5213a7db3068254b1fb428d631d952420c7c1c7d
crisstinagl <crisgdl3011@gmail.com>
2025-04-29 12:23:13 +0200

    Nombres + plantilla

---
+++ G5.m
@@ -0,0 +1,57 @@
+%% Introducción a los Métodos Matemáticos y Numéricos
+%% Plantilla para la entrega de la práctica realizada en grupo 
+%% Curso 2024-2024
+                    
+%% Escribir (todos y cada uno de los miembros del grupo)
+% (desarrollador 1)
+% Nombre completo: Alexia 
+% Apellidos completos: Fernández Millán
+
+% (desarrollador 2)
+% Nombre completo: Claudia
+% Apellidos completos: Porcuna Rivas
+
+% (desarrollador 3)
+% Nombre completo: Cristina 
+% Apellidos completos: González de Lope
+
+% (desarrollador 4)
+% Nombre completo: Karina Diana
+% Apellidos completos: Hincu
+
+% (desarrollador 5)
+% Nombre completo: Alexandra 
+% Apellidos completos: Alina Pop 
+
+
+%% Nivel 1 [12 puntos]
+clear 
+close all
+clc 
+
+%% 1) [5 puntos]
+%% Solución:
+%% 2) [2 puntos]
+%% Solución:
+%% 3) [5 puntos]
+%% Solución:
+
+%% NIVEL 2: [8 puntos]
+
+%% 1) [2 puntos]
+%% Solución:
+%% 2) [5 puntos]
+%% Solución:
+%% 3) [1 punto]
+%% Solución:
+
+
+%% NIVEL 3: [5 puntos]
+
+%% Solución:
+
+%% NIVEL 4 (Opcional): [3 puntos extra]
+
+%% Solución:
+
+
